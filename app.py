b4b11eeb84153b128bd866ae2dd48d62d708eca6
Dasol <kr1367@nate.com>
2022-03-01 17:53:04 +0900

    test-chrome-driver

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from collect import selenium_driver
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
+    selenium_driver.do_collect();
     return 'Hello World!'
 
 
